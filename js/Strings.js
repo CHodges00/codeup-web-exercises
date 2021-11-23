404b05d5b75d5849bf3366f7035aec15a437622b
Cody Hodges <hodges.cody00@gmail.com>
2021-11-23 13:11:41 -0600

    Strings Exercises

---
+++ js/Strings.js
@@ -0,0 +1,22 @@
+var myName = "Hi,\nmy name is Cody. ";
+console.log(myName);
+
+var myLiving = "I am currently living in\nSan Antonio.";
+console.log(myLiving);
+
+var myHopes = "I hope to be back in-person ASAP !";
+console.log(myHopes);
+
+console.log(myName + myLiving + myHopes);
+
+let newName = "doug";
+let firstletter = newName.charAt(0).toUpperCase();
+newName = newName.substr(1, 4);
+console.log(firstletter + newName);
+
+
+
+
+
+
+
