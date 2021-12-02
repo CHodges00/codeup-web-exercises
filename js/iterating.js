c2400436194b4b0c8c1357cbdfbceba30e7931bc
Cody Hodges <hodges.cody00@gmail.com>
2021-12-01 22:42:13 -0600

    Arrays Exercises

--- js/iterating.js
+++ js/iterating.js
@@ -1,4 +1,4 @@
-(function(){
+(function() {
     "use strict";
 
     /**
@@ -7,11 +7,15 @@
      * 'names'.
      */
 
+    var names = ['cody', 'danie', 'greg', 'wendy']
+
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
      */
+
+    console.log(names.length);
 
     /**
      * TODO:
@@ -19,17 +23,30 @@
      * accessing each element's index.
      */
 
+    console.log(names[0]);
+    console.log(names[1]);
+    console.log(names[2]);
+    console.log(names[3]);
+
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
 
+    for (var i = 0; i < names.length; i++) {
+        console.log(names[i]);
+    }
+
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
 
+    names.forEach(function(name) {
+        console.log(name);
+    });
+})();
     /**
      * TODO:
      * Create the following three functions, each will accept an array and
@@ -43,5 +60,5 @@
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
+function 
 
-})();
