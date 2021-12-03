e4369a66516aeb1c75c341e3cc464ca14c09278c
Cody Hodges <hodges.cody00@gmail.com>
2021-12-02 19:50:42 -0600

    Arrays Exercises

--- js/planets-array.js
+++ js/planets-array.js
@@ -17,24 +17,49 @@
      * Read each console log below, and write some javascript code to perform
      * the step that it describes
      */
-
+    planets.unshift('The Sun');
     console.log('Adding "The Sun" to the beginning of the planets array.');
     console.log(planets);
 
+
+
+    planets.push('Pluto');
     console.log('Adding "Pluto" to the end of the planets array.');
     console.log(planets);
 
+
+
+
+
+    planets.shift();
     console.log('Removing "The Sun" from the beginning of the planets array.');
     console.log(planets);
 
+
+
+
+    planets.pop ();
     console.log('Removing "Pluto" from the end of the planets array.');
     console.log(planets);
 
-    console.log('Finding and logging the index of "Earth" in the planets array.');
 
+
+
+
+    console.log('Finding and logging the index of "Earth" in the planets array.');
+    var index = planets.indexOf('Earth');
+    console.log(index);
+
+
+
+    planets.reverse ();
     console.log("Reversing the order of the planets array.");
     console.log(planets);
 
+
+
+
+    planets.sort ();
     console.log("Sorting the planets array.");
     console.log(planets);
 })();
