0a1d9792f992f088f14efc6d8b3e2b22ac8dfd92
Cody Hodges <hodges.cody00@gmail.com>
2021-12-05 12:46:39 -0600

    Objects Exercises

--- js/circle.js
+++ js/circle.js
@@ -15,25 +15,26 @@
             // TODO: complete this method.
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
-            if(doRounding) {
-                console.log(Math.round(this.getArea()));
+            if (doRounding === true){
+                return (Math.round(this.getArea()))
             }else
-                console.log("Area of a circle with radius: " + this.radius + ", is: " + this.getArea());
+                return (this.getArea());
         }
     };
 
     // log info about the circle
     console.log("Raw circle information");
-    circle.logInfo(false);
+    console.log(circle.logInfo(false));
     console.log("Circle information rounded to the nearest whole number");
-    circle.logInfo(true);
+    console.log(circle.logInfo(true));
 
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
     circle.radius = 5;
     // log info about the circle
+
     console.log("Raw circle information");
-    circle.logInfo(false);
+    console.log(circle.logInfo(false));
     console.log("Circle information rounded to the nearest whole number");
-    circle.logInfo(true);
+    console.log(circle.logInfo(true));
 })();
