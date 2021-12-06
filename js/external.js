ffc14e69878df347560d957786b49cb08fe10fac
Cody Hodges <hodges.cody00@gmail.com>
2021-12-06 16:45:31 -0600

    JS/HTML Exercises

---
+++ js/external.js
@@ -0,0 +1,8 @@
+"use strict";
+
+console.log("Hello from external JavaScript");
+
+alert('Welcome to my Website!');
+
+var color = prompt('What is your fav color?');
+alert(color + '! Thats my fav too!')
