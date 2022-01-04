6d7c7894e549d98b5e622ff76d6899735752ef4e
Cody Hodges <hodges.cody00@gmail.com>
2022-01-04 13:31:45 -0600

    Pizza Page

---
+++ js/pizza.js
@@ -0,0 +1,12 @@
+$('#next').click(function () {
+    $('#profile-tab').tab('show');
+});
+
+$('#previous').click(function (){
+    $('#home-tab').tab('show');
+});
+
+$('#next-step').click(function (){
+    $('#contact-tab').tab('show');
+});
+
