7058688e613e9de7c9edc5792d280a98a5d1c9ba
Cody Hodges <hodges.cody00@gmail.com>
2022-01-10 16:17:06 -0600

    Functions Practice

---
+++ js/JS-Practice.js
@@ -0,0 +1,39 @@
+
+// RETURN TIMES 3
+function timesThree (a) {
+    return (a * 3);
+}
+
+console.log('numbers')
+    console.log(timesThree(4))
+    console.log(timesThree(28))
+    console.log(timesThree(2))
+    console.log(timesThree(80))
+console.log('string')
+    console.log(timesThree('4'));
+    console.log(timesThree('28'));
+    console.log(timesThree('2'));
+    console.log(timesThree('80'));
+console.log('next function')
+
+
+// RETURN 'NOT A NUMBER' IF NOT NUM OR NUM STRING
+
+    function timesThreeString (a) {
+        if (a ==) {
+            return (a * 3);
+        } else {
+            return ('not a number');
+        }
+    }
+
+console.log(timesThreeString(2))
+console.log(timesThreeString(80))
+console.log(timesThreeString('tom'));
+console.log(timesThreeString('?'));
+
+
+// TAKE IN NUM AND STRING RETURN STRING 'MATCHES LENGTH' OR 'NO MATCH' IF NUMBER = STRING LENGTH
+
+
+// FUNCTION DAY OF WEEK AS STRING RETURN NUMBER THAT MATCHES DAY OF WEEK SUNDAY>1-7<SATURDAY
