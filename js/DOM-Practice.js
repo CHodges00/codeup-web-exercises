a33875ec2b4aaa5d1cb096bc0c5d621cb6c45a5f
Cody Hodges <hodges.cody00@gmail.com>
2022-01-13 08:52:25 -0600

    DOM Practice

--- js/DOM-Practice.js
+++ js/DOM-Practice.js
@@ -1,17 +1,19 @@
 
 // ADD NAME INPUT IN PROMPT
-var name = prompt("what's your name ?")
-var newName = document.getElementById('name');
+// var name = prompt("what's your name ?")
+// var newName = document.getElementById('name');
+//
+// newName.innerHTML = name;
+//
+//
+// var oldP = document.getElementsByClassName('para');
+// var newP = 'Cloudy'
+//
+// for (var i = 0; i < oldP.length; i++){
+//     oldP[i].innerHTML = newP
+// }
 
-newName.innerHTML = name;
 
-
-var oldP = document.getElementsByClassName('para');
-var newP = 'Cloudy'
-
-for (var i = 0; i < oldP.length; i++){
-    oldP[i].innerHTML = newP
-}
 
 
 
