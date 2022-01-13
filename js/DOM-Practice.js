cbcc7d557397f7733475c9f8ceb6fd7df4da2a76
Cody Hodges <hodges.cody00@gmail.com>
2022-01-13 12:36:57 -0600

    DOM Prcatice

--- js/DOM-Practice.js
+++ js/DOM-Practice.js
@@ -1,18 +1,3 @@
-// ADD NAME INPUT IN PROMPT
-// var name = prompt("what's your name ?")
-// var newName = document.getElementById('name');
-//
-// newName.innerHTML = name;
-//
-//
-// var oldP = document.getElementsByClassName('para');
-// var newP = 'Cloudy'
-//
-// for (var i = 0; i < oldP.length; i++){
-//     oldP[i].innerHTML = newP
-// }
-
-
 let movies = function () {
     [
         {title: 'HAPPY FEET TWO', image: "assets/movie1.jpeg", release: '2011', gross: '$157,956,466'},
@@ -25,33 +10,33 @@ let movies = function () {
         {title: 'wertyui', image: "assets/movie8.jpeg", release: '4567', gross: '$345,567,567'}
     ]
 }
-
-
-
-function createHTML(movie) {
-    let html = '<div class="col-5 movies"><h2>"' + movie.title + '"</h2><img src="' + movie.image + '"><p>"' + movie.release + '"</p><p>"' + movie.gross + '"</p></div>'
-    return html;
-}
-
-
-
-function createColumns(movies) {
-    let html = '';
-    for (let i = 0; i < movies.length; i++) {
-        html += createHTML(movies[i]);
-    }
-    return html;
-}
-
-
-
-
-let contentArea = document.getElementsByClassName('row');
-contentArea.innerHTML = createColumns(movies);
-
-let setMovies = document.getElementById('btn-one')
-setMovies.addEventListener('click', createColumns);
-
+//
+//
+//
+// function createHTML(movie) {
+//     let html = '<div class="col-5 movies"><h2>"' + movie.title + '"</h2><img src="' + movie.image + '"><p>"' + movie.release + '"</p><p>"' + movie.gross + '"</p></div>'
+//     return html;
+// }
+//
+//
+//
+// function createColumns(movies) {
+//     let html = '';
+//     for (let i = 0; i < movies.length; i++) {
+//         html += createHTML(movies[i]);
+//     }
+//     return html;
+// }
+//
+//
+//
+//
+// let contentArea = document.getElementsByClassName('row');
+// contentArea.innerHTML = createColumns(movies);
+//
+// let setMovies = document.getElementById('btn-one')
+// setMovies.addEventListener('click', createColumns);
+//
 
 
 
