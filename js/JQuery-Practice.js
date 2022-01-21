4f70c64b3f63f9a340ecc7a70c99b6fa861add61
Cody Hodges <hodges.cody00@gmail.com>
2022-01-21 08:53:00 -0600

    JQuery Practice

--- js/JQuery-Practice.js
+++ js/JQuery-Practice.js
@@ -1,30 +1,100 @@
-let para = $('#one');
-console.log(para);
+// let para = $('#one');
+// console.log(para);
+//
+//
+// // GET ELEMENT
+// let header = $('.header');
+// console.log(header);
+// // GET TEXT
+// let textOfHeader = $('.header').text()
+// console.log(textOfHeader)
+// // GET HTML/TEXT
+// let htmlOfHeader = $('.header').html()
+// console.log(htmlOfHeader)
+//
+// // ADD CLASS TO ELEMENT
+// // $('.header').addClass('border');
+//
+//
+//
+// // IF LOGGED IN WILL BE RED
+// let usr1 = {
+//     name: 'tom',
+//     loggedIn : false,
+//     age: 30
+// }
+//
+// let loginStatus = usr1.loggedIn
+// if (loginStatus === true){
+//     $('.header').addClass('border')
+// }
 
 
-// GET ELEMENT
-let header = $('.header');
-console.log(header);
-// GET TEXT
-let textOfHeader = $('.header').text()
-console.log(textOfHeader)
-// GET HTML/TEXT
-let htmlOfHeader = $('.header').html()
-console.log(htmlOfHeader)
-
-// ADD CLASS TO ELEMENT
-// $('.header').addClass('border');
 
 
 
-// IF LOGGED IN WILL BE RED
-let usr1 = {
-    name: 'tom',
-    loggedIn : false,
-    age: 30
-}
 
-let loginStatus = usr1.loggedIn
-if (loginStatus === true){
-    $('.header').addClass('border')
-}
+////////////////   LAURA PRACTICE //////////////////
+
+// 1. give all table cells a black border
+
+
+
+// 2. remove all bullet points from the unordered list items
+
+
+
+// 3. change the font family of the entire page to luxurious roman
+// ---> <link rel=“preconnect” href=“https://fonts.googleapis.com”>
+//     <link rel=“preconnect” href=“https://fonts.gstatic.com” crossorigin>
+//     <link href=“https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Questrial&display=swap” rel=“stylesheet”>
+//     --->font-family: ‘Abril Fatface’, cursive;
+// font-family: ‘Questrial’, sans-serif;
+
+
+
+
+// 4. give each section a unique id
+
+
+
+// 5. using the unique id’s give each section a different background color
+
+
+
+// 6. give 3 elements the same class name of “highlight”
+
+
+
+// 7. give each of those 3 elements a bright yellow background
+
+
+
+// 8. BONUS: when you click on the image, make the image larger than what it currently is
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
