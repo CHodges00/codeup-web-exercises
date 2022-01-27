c7e0bb5ff6cdbda5f2b52cc2f9d3385aa03e6d55
Cody Hodges <hodges.cody00@gmail.com>
2022-01-27 16:31:53 -0600

    WEATHER MAP Project

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,20 +1,61 @@
+// ON SUBMIT BUTTON VLIVK
+$('#submit').on('click', function () {
 
-
-$('#submit').on('click', function (){
+// CITY = USERS INPUT VAL
     var city = $('#city').val();
-
-
+// GET DATA FOR CITY NAME ENTERED
     $.get("http://api.openweathermap.org/data/2.5/weather", {
         APPID: 'dc7b0ebe3a51ce3421b1dfcff8a97724',
-        q:  city
-    }).done(function(data) {
-        console.log(data);
+        q: city
+// WHEN DONE -----
+    }).done(function (data) {
+        console.log(data)
+        $('#cityName').append(`<p>Current City: ${data.name}`);
+
+        $('.row').append(`<div class="col-2 ml-auto mr-auto">
+        <div class="card">
+            <h5 class="card-header text-center">DATE</h5>
+            <div class="card-body">
+                <div class="card-text">
+                    <div class="card-center">
+                        <p>TEMPS</p>
+                        <img src='http://openweathermap.org/img/w/04n.png' style="width: 30px; height: 30px">
+                    </div>
+                    <hr>
+                        <p>Description: <b>OVERCAST CLOUDS</b></p>
+                        <p>Humidity: <b>HUMIDITY</b></p>
+                        <hr>
+                            <p>Wind: <b>WIND</b></p>
+                            <hr>
+                                <p>Pressure: <b>PRESSURE</b></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="map"></div>`)
     });
-
-
 })
 
 
+// // console.log(data.coord);
+// mapboxgl.accessToken = 'pk.eyJ1IjoiaG9kZ2VzY29keTAwIiwiYSI6ImNreXhoeHBhYTBpNTYyb3FwNG90aWFkaGIifQ.kYy3FfTNOUM0hO486IGMZQ';
+//
+//
+//
+// let map = new mapboxgl.Map({
+//     container: 'map', // container ID
+//     style: 'mapbox://styles/mapbox/streets-v11', // style URL
+//     center: {lon: -47.23, lat: 47.23}, // starting position [lng, lat]
+//     zoom: 10 // starting zoom
+// });
+// console.log(data.coord)
+// console.log(map.center)
+//
+//
+// let marker = new mapboxgl.Marker()
+//     .setLngLat()
+//     .setPopup(new mapboxgl.Popup().setHTML(`${restaurant.name}<br>${restaurant.Hours}<br>Best Food: ${restaurant.bestFood}`)) // add popup
+//     .addTo(map);
 
 
 
