4571f9831c71c3505d14dbfc52d8fcceecb9b9b9
Cody Hodges <hodges.cody00@gmail.com>
2022-01-31 13:31:39 -0600

    Weather Map Exercise

--- js/TOKENS.js
+++ js/TOKENS.js
@@ -1,1 +1,4 @@
-const MAPBOX_KEY = 'dc7b0ebe3a51ce3421b1dfcff8a97724';
+
+const MAPBOX_TOKEN = "pk.eyJ1IjoiaG9kZ2VzY29keTAwIiwiYSI6ImNreXVtOG5odDFvc2EycHBlaHhpMzF1c3QifQ.6g0ZKujuRweI-lJkBKuWAQ";
+
+const WEATHER_TOKEN = "dc7b0ebe3a51ce3421b1dfcff8a97724";
--- js/weather_map.js
+++ js/weather_map.js
@@ -1,60 +1,82 @@
-// ON SUBMIT BUTTON VLIVK
+// ON SUBMIT BUTTON
 $('#submit').on('click', function () {
+
 
 // CITY = USERS INPUT VAL
     var city = $('#city').val();
+
 // GET DATA FOR CITY NAME ENTERED
-    $.get("http://api.openweathermap.org/data/2.5/weather", {
-        APPID: 'dc7b0ebe3a51ce3421b1dfcff8a97724',
-        q: city
+    $.get("http://api.openweathermap.org/data/2.5/forecast", {
+        APPID: "dc7b0ebe3a51ce3421b1dfcff8a97724",
+        q: city,
+        units: "imperial"
 // WHEN DONE -----
     }).done(function (data) {
-        console.log(data)
-        $('#cityName').append(`<p>Current City: ${data.name}`);
-        console.log(data.main)
-        $('.row').append(`<div class="col-2 ml-auto mr-auto">
-        <div class="card">
-            <h5 class="card-header text-center">DATE</h5>
-            <div class="card-body">
-                <div class="card-text">
-                    <div class="card-center">
-                        <p>${data.main.temp}<br>${data.main.feels_like}</p>
-                        <img src='http://openweathermap.org/img/w/04n.png' style="width: 30px; height: 30px">
-                    </div>
-                    <hr>
-                        <p>Description: <b>OVERCAST CLOUDS</b></p>
-                        <p>Humidity: <b>HUMIDITY</b></p>
-                        <hr>
-                            <p>Wind: <b>WIND</b></p>
-                            <hr>
-                                <p>Pressure: <b>PRESSURE</b></p>
-                </div>
-            </div>
-        </div>
-    </div>`)
-    });
+        // console.log(data)
+
+        for (var i = 0; i < 5; i++){
+            var day = data.list[i];
+            var date = day.dt_txt;
+            console.log(date)
+            date = date.substring(0, 10);
+
+            console.log(date)
+            // console.log(day)
+            $('.row').append(`<div class="col-2 ml-auto mr-auto">
+                            <div class="card">
+                                <h5 class="card-header text-center">${date}</h5>
+                                <div class="card-body">
+                                    <div class="card-text">
+                                        <div class="card-center">
+                                        <div class="temps" style="line-height: 3">
+                                           <p>HIGH: ${day.main.temp_max}F<br>LOW: ${day.main.temp_min}F</p>
+                                        </div>
+                                        <img src='http://openweathermap.org/img/w/${day.weather[0].icon}.png' style="width: 30px; height: 30px">
+                                        </div>
+                                    <hr>
+                                    <p>Description: <b>${day.weather[0].description}</b></p>
+                                    <p>Humidity: <b>${day.main.humidity} %</b></p>
+                                    <hr>
+                                    <p>Wind: <b>${day.wind.speed} mph</b></p>
+                                    <hr>
+                                    <p>Pressure: <b>${day.main.pressure}</b></p>
+                                    </div>
+                                </div>
+                            </div>
+                            </div>`);
+        }
+
+        $('#cityName').append(`<p>Current City: ${data.city.name}`);
+
+
+
+
+    //
+    //     mapboxgl.accessToken = 'pk.eyJ1IjoiaG9kZ2VzY29keTAwIiwiYSI6ImNrejJ3ZWhmcjAweGoybm55Z3lrNTlyNWgifQ.2Y_WT2W1PWBdIJCuK9azig';
+    //
+    //     var map = new mapboxgl.Map({
+    //         container: 'map', // container ID
+    //         style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    //         center: data.coord, // starting position [lng, lat]
+    //         zoom: 10 // starting zoom
+    //     });
+    //     let marker = new mapboxgl.Marker()
+    //         .setLngLat(data.coord)
+    //         .setPopup(new mapboxgl.Popup().setHTML(`${data.name}`)) // add popup
+    //         .addTo(map);
+    //
+    //
+    })
+
+
+
+
 })
 
 
-// // console.log(data.coord);
-// mapboxgl.accessToken = 'pk.eyJ1IjoiaG9kZ2VzY29keTAwIiwiYSI6ImNreXhoeHBhYTBpNTYyb3FwNG90aWFkaGIifQ.kYy3FfTNOUM0hO486IGMZQ';
-//
-//
-//
-// let map = new mapboxgl.Map({
-//     container: 'map', // container ID
-//     style: 'mapbox://styles/mapbox/streets-v11', // style URL
-//     center: {lon: -47.23, lat: 47.23}, // starting position [lng, lat]
-//     zoom: 10 // starting zoom
-// });
-// console.log(data.coord)
-// console.log(map.center)
-//
-//
-// let marker = new mapboxgl.Marker()
-//     .setLngLat()
-//     .setPopup(new mapboxgl.Popup().setHTML(`${restaurant.name}<br>${restaurant.Hours}<br>Best Food: ${restaurant.bestFood}`)) // add popup
-//     .addTo(map);
+
+
+
 
 
 
