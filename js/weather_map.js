718c8ff33e1d5e3abbf6ec2a9638f968c32bc967
Cody Hodges <hodges.cody00@gmail.com>
2022-02-01 12:40:51 -0600

    Weather Map Project

--- js/weather_map.js
+++ js/weather_map.js
@@ -227,90 +227,27 @@ $.get("http://api.openweathermap.org/data/2.5/forecast", {
     });
 
     var marker = new mapboxgl.Marker({
-        draggable: true
+        color: 'red',
+        draggable: true,
+
     })
+        
         .setLngLat(data.city.coord)
         .setPopup(new mapboxgl.Popup().setHTML(`${data.city.name}`))
+
         .addTo(map);
 
 
 
-//
-//     const geocoder = new MapboxGeocoder({
-//         placeholder: 'Search your favorite spot',
-//         // Initialize the geocoder
-//         accessToken: mapboxgl.accessToken, // Set the access token
-//         mapboxgl: mapboxgl, // Set the mapbox-gl instance
-//         marker: true // Do not use the default marker style,
-//
-//     });
-//
-// // Add the geocoder to the map
-//     map.addControl(geocoder);
-//
-//     geocoder.on('result', (event) => {
-//         var lngLat = marker.getLngLat();
-//         console.log(lngLat)
-//         var lat = lngLat.lat
-//         var lon = lngLat.lng
-//
-//
-//         $.get("http://api.openweathermap.org/data/2.5/forecast", {
-//             APPID: "dc7b0ebe3a51ce3421b1dfcff8a97724",
-//             lat:  lat ,
-//             lon:  lon ,
-//             units: "imperial"
-// // WHEN DONE -----
-//         }).done(function (data) {
-//             console.log(data)
-//             $("#cityName").html('')
-//             $('#cityName').append(`<p>Current City: ${data.city.name}`);
-//             $('.row').html('')
-//
-//             for (var i = 0; i < 40; i += 8) {
-//
-// /////////////////////////////////////// GENERATE WEATHER CARDS ///////////////////////////////////////
-//                 $('.row').append(`<div class="col-2 ml-auto mr-auto">
-//                             <div class="card">
-//                                 <h5 class="card-header text-center">${data.list[i].dt_txt.substring(0, 10)}</h5>
-//                                 <div class="card-body">
-//                                     <div class="card-text">
-//                                         <div class="card-center">
-//                                         <div class="temps" style="line-height: 3">
-//                                            <p style="font-size: 20px; font-weight: bolder">${data.list[i].main.temp_min}F</p>
-//                                         </div>
-//                                         <img src='http://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png' style="width: 30px; height: 30px">
-//                                         </div>
-//                                     <hr>
-//                                     <p>Description: <b>${data.list[i].weather[0].description}</b></p>
-//                                     <p>Humidity: <b>${data.list[i].main.humidity} %</b></p>
-//                                     <hr>
-//                                     <p>Wind: <b>${data.list[i].wind.speed} mph</b></p>
-//                                     <hr>
-//                                     <p>Pressure: <b>${data.list[i].main.pressure}</b></p>
-//                                     </div>
-//                                 </div>
-//                             </div>
-//                             </div>`);
-//             }
-//         })
-//
-//     });
 
 
-
-
-
-
-/////////////////////////////////////// DRAGGABLE MARKER THAT RETURNS LNGLAT ///////////////////////////////////////
+/////////////////////////////////////// DRAGABLE MARKER THAT RETURNS LNGLAT ///////////////////////////////////////
     function onDragEnd() {
         var lngLat = marker.getLngLat();
         coordinates.innerHTML = `Longitude: ${lngLat.lng}<br />Latitude: ${lngLat.lat}`;
         var lat = lngLat.lat
         var lon = lngLat.lng
-        console.log(lngLat)
-
-
+        // console.log('DRAG MARKER LNGLAT ' + lngLat)
 
 
         map.flyTo({
@@ -335,17 +272,14 @@ $.get("http://api.openweathermap.org/data/2.5/forecast", {
         });
 
 
-
-
-
         $.get("http://api.openweathermap.org/data/2.5/forecast", {
             APPID: "dc7b0ebe3a51ce3421b1dfcff8a97724",
-            lat:  lat ,
-            lon:  lon ,
+            lat: lat,
+            lon: lon,
             units: "imperial"
 // WHEN DONE -----
         }).done(function (data) {
-            console.log(data)
+            // console.log('DRAG MARKER CITY DATA ' + data)
             $("#cityName").html('')
             $('#cityName').append(`<p>Current City: ${data.city.name}`);
             $('.row').html('')
@@ -378,55 +312,73 @@ $.get("http://api.openweathermap.org/data/2.5/forecast", {
             }
         })
 
-        }
+    }
 
 
     marker.on('dragend', (onDragEnd));
 
 
 
-    // GEOCODER GEODER GEOCODER
-const geocoder = new MapboxGeocoder({
-    placeholder: 'Search your favorite spot',
-    // Initialize the geocoder
-    accessToken: mapboxgl.accessToken, // Set the access token
-    mapboxgl: mapboxgl, // Set the mapbox-gl instance
-    marker: false // Do not use the default marker style,
 
-});
+ ///////////////////////   GEOCODER    ///////////////////////
+    const geocoder = new MapboxGeocoder({
+        placeholder: 'Search your favorite spot',
+        // Initialize the geocoder
+        accessToken: mapboxgl.accessToken, // Set the access token
+        mapboxgl: mapboxgl, // Set the mapbox-gl instance
+        marker: false // Do not use the default marker style
+    });
 
 // Add the geocoder to the map
-map.addControl(geocoder);
+    map.addControl(geocoder);
+
+    // After the map style has loaded on the page,
+// add a source layer and default styling for a single point
+    map.on('load', () => {
+        map.addSource('single-point', {
+            type: 'geojson',
+            data: {
+                type: 'FeatureCollection',
+                features: []
+            }
+        });
+
+        map.addLayer({
+            id: 'point',
+            source: 'single-point',
+            type: 'circle',
+            paint: {
+                'circle-radius': 10,
+                'circle-color': '#448ee4'
+            }
+        });
+
+        // Listen for the `result` event from the Geocoder
+        // `result` event is triggered when a user makes a selection
+        //  Add a marker at the result's coordinates
+        geocoder.on('result', (event) => {
+            map.getSource('single-point').setData(event.result.geometry);
+            console.log(event)
+            var lon = event.result.center[0]
+            var lat = event.result.center[1]
 
 
-
-
-
-
-geocoder.on('result', (event) => {
-    var lngLat = marker.getLngLat();
-    console.log(lngLat)
-    var lat = lngLat.lat
-    var lon = lngLat.lng
-
-
-
-    $.get("http://api.openweathermap.org/data/2.5/forecast", {
-        APPID: "dc7b0ebe3a51ce3421b1dfcff8a97724",
-        lat:  lat ,
-        lon:  lon ,
-        units: "imperial"
+        $.get("http://api.openweathermap.org/data/2.5/forecast", {
+            APPID: "dc7b0ebe3a51ce3421b1dfcff8a97724",
+            lat: lat,
+            lon: lon,
+            units: "imperial"
 // WHEN DONE -----
-    }).done(function (data) {
-        console.log(data)
-        $("#cityName").html('')
-        $('#cityName').append(`<p>Current City: ${data.city.name}`);
-        $('.row').html('')
+        }).done(function (data) {
+            // console.log('DRAG MARKER CITY DATA ' + data)
+            $("#cityName").html('')
+            $('#cityName').append(`<p>Current City: ${data.city.name}`);
+            $('.row').html('')
 
-        for (var i = 0; i < 40; i += 8) {
+            for (var i = 0; i < 40; i += 8) {
 
 /////////////////////////////////////// GENERATE WEATHER CARDS ///////////////////////////////////////
-            $('.row').append(`<div class="col-2 ml-auto mr-auto">
+                $('.row').append(`<div class="col-2 ml-auto mr-auto">
                             <div class="card">
                                 <h5 class="card-header text-center">${data.list[i].dt_txt.substring(0, 10)}</h5>
                                 <div class="card-body">
@@ -448,83 +400,10 @@ geocoder.on('result', (event) => {
                                 </div>
                             </div>
                             </div>`);
-        }
-    })
+            }
+        })
 
+    });
+    });
 });
-})
 
-
-
-
-
-
-// // GEOCODER GEODER GEOCODER
-// const geocoder = new MapboxGeocoder({
-//     placeholder: 'Search your favorite spot',
-//     // Initialize the geocoder
-//     accessToken: mapboxgl.accessToken, // Set the access token
-//     mapboxgl: mapboxgl, // Set the mapbox-gl instance
-//     marker: true // Do not use the default marker style,
-//
-// });
-//
-// // Add the geocoder to the map
-// map.addControl(geocoder);
-//
-// geocoder.on('result', (event) => {
-//     var lngLat = marker.getLngLat();
-//     console.log(lngLat)
-//     var lat = lngLat.lat
-//     var lon = lngLat.lng
-//
-//
-//     $.get("http://api.openweathermap.org/data/2.5/forecast", {
-//         APPID: "dc7b0ebe3a51ce3421b1dfcff8a97724",
-//         lat:  lat ,
-//         lon:  lon ,
-//         units: "imperial"
-// // WHEN DONE -----
-//     }).done(function (data) {
-//         console.log(data)
-//         $("#cityName").html('')
-//         $('#cityName').append(`<p>Current City: ${data.city.name}`);
-//         $('.row').html('')
-//
-//         for (var i = 0; i < 40; i += 8) {
-//
-// /////////////////////////////////////// GENERATE WEATHER CARDS ///////////////////////////////////////
-//             $('.row').append(`<div class="col-2 ml-auto mr-auto">
-//                             <div class="card">
-//                                 <h5 class="card-header text-center">${data.list[i].dt_txt.substring(0, 10)}</h5>
-//                                 <div class="card-body">
-//                                     <div class="card-text">
-//                                         <div class="card-center">
-//                                         <div class="temps" style="line-height: 3">
-//                                            <p style="font-size: 20px; font-weight: bolder">${data.list[i].main.temp_min}F</p>
-//                                         </div>
-//                                         <img src='http://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png' style="width: 30px; height: 30px">
-//                                         </div>
-//                                     <hr>
-//                                     <p>Description: <b>${data.list[i].weather[0].description}</b></p>
-//                                     <p>Humidity: <b>${data.list[i].main.humidity} %</b></p>
-//                                     <hr>
-//                                     <p>Wind: <b>${data.list[i].wind.speed} mph</b></p>
-//                                     <hr>
-//                                     <p>Pressure: <b>${data.list[i].main.pressure}</b></p>
-//                                     </div>
-//                                 </div>
-//                             </div>
-//                             </div>`);
-//         }
-//     })
-//
-// });
-//
-//
-//
-//
-//
-//
-//
-//
