28914776e30260ac99d87aabf38d0225a02fced7
Cody Hodges <hodges.cody00@gmail.com>
2022-02-02 11:52:55 -0600

    Weather Map Project

--- js/weather_map.js
+++ js/weather_map.js
@@ -17,7 +17,7 @@ function cards(){
 
 ///////////////////////////////////////// GENERATE FIRST MAP/WEATHER FOR SAN ANTONIO ///////////////////////////////////////
 $.get("http://api.openweathermap.org/data/2.5/forecast", {
-    APPID: "dc7b0ebe3a51ce3421b1dfcff8a97724",
+    APPID: WEATHER_TOKEN,
     q: "San Antonio",
     units: "imperial"
 // WHEN DONE -----
@@ -55,7 +55,7 @@ $.get("http://api.openweathermap.org/data/2.5/forecast", {
 
 
     /////////////////////////////////////// GENERATE MAP ///////////////////////////////////////
-    mapboxgl.accessToken = 'pk.eyJ1IjoiaG9kZ2VzY29keTAwIiwiYSI6ImNrejJ3ZWhmcjAweGoybm55Z3lrNTlyNWgifQ.2Y_WT2W1PWBdIJCuK9azig';
+    mapboxgl.accessToken = MAPBOX_TOKEN;
     var coordinates = document.getElementById('coordinates');
     var map = new mapboxgl.Map({
         container: 'map',
@@ -95,7 +95,7 @@ $.get("http://api.openweathermap.org/data/2.5/forecast", {
 
 
         $.get("http://api.openweathermap.org/data/2.5/forecast", {
-            APPID: "dc7b0ebe3a51ce3421b1dfcff8a97724",
+            APPID: WEATHER_TOKEN,
             lat: lat,
             lon: lon,
             units: "imperial"
@@ -183,7 +183,7 @@ $.get("http://api.openweathermap.org/data/2.5/forecast", {
 
 
             $.get("http://api.openweathermap.org/data/2.5/forecast", {
-                APPID: "dc7b0ebe3a51ce3421b1dfcff8a97724",
+                APPID: WEATHER_TOKEN,
                 lat: lat,
                 lon: lon,
                 units: "imperial"
@@ -224,5 +224,126 @@ $.get("http://api.openweathermap.org/data/2.5/forecast", {
     });
 });
 
+
+
+
+
+
+
+///////////////////////// CROSSWORD /////////////////////////
+//Globals
+var currentTextInput;
+var puzzleArrayData;
+//Loads the Crossword
+function initializeScreen(){
+    var puzzleTable = document.getElementById("puzzle");
+    puzzleArrayData = preparePuzzleArray();
+    for ( var i = 0; i < puzzleArrayData.length ; i++ ) {
+        var row = puzzleTable.insertRow(-1);
+        var rowData = puzzleArrayData[i];
+        for(var j = 0 ; j < rowData.length ; j++){
+            var cell = row.insertCell(-1);
+            if(rowData[j] != 0){
+                var txtID = String('txt' + '_' + i + '_' + j);
+                cell.innerHTML = '<input type="text" class="inputBox" maxlength="1" style="text-transform: lowercase" ' + 'id="' + txtID + '" onfocus="textInputFocus(' + "'" + txtID + "'"+ ')">';
+            }else{
+                cell.style.backgroundColor  = "black";
+            }
+        }
+    }
+    addHint();
+}
+//Adds the hint numbers
+function addHint(){
+    document.getElementById("txt_0_9").placeholder = "3";
+    document.getElementById("txt_4_4").placeholder = "2";
+    document.getElementById("txt_3_1").placeholder = "1";
+    document.getElementById("txt_4_0").placeholder = "5";
+    document.getElementById("txt_1_6").placeholder = "4";
+    document.getElementById("txt_7_3").placeholder = "6";
+}
+//Stores ID of the selected cell into currentTextInput
+function textInputFocus(txtID123){
+    currentTextInput = txtID123;
+}
+//Returns Array
+function preparePuzzleArray(){
+    var items = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 'g'],
+        [0, 0, 0, 0, 0, 0, 'f', 'o', 'u', 'r' ],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 'e'],
+        [0, 'w', 0, 0, 0, 0, 0, 0, 0, 'a'],
+        ['j', 'a', 'v', 'a', 's', 'c', 'r', 'i', 'p', 't'],
+        [0, 't', 0, 0, 'c', 0, 0, 0, 0, 0],
+        [0, 'e', 0, 0, 'r', 0, 0, 0, 0, 0],
+        [0, 'r', 0, 'p', 'u', 'b', 'g', 0, 0, 0],
+        [0, 0, 0, 0, 'b', 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ];
+    return items;
+}
+//Clear All Button
+function clearAllClicked(){
+    currentTextInput = '';
+    var puzzleTable = document.getElementById("puzzle");
+    puzzleTable.innerHTML = '';
+    initializeScreen();
+}
+//Check button
+function checkClicked(){
+    for ( var i = 0; i < puzzleArrayData.length ; i++ ) {
+        var rowData = puzzleArrayData[i];
+        for(var j = 0 ; j < rowData.length ; j++){
+            if(rowData[j] != 0){
+                var selectedInputTextElement = document.getElementById('txt' + '_' + i + '_' + j);
+                if(selectedInputTextElement.value != puzzleArrayData[i][j]){
+                    selectedInputTextElement.style.backgroundColor = 'yellow';
+
+                }else{
+                    selectedInputTextElement.style.backgroundColor = 'green';
+                }
+            }
+        }
+    }
+}
+
+//Solve Button
+function solveClicked(){
+    if (currentTextInput != null){
+        var temp1 = currentTextInput;
+        var token = temp1.split("_");
+        var row = token[1];
+        var column = token[2];
+
+        // Print elements on top
+        for(j = row; j >= 0; j--){
+            if(puzzleArrayData[j][column] != 0){
+                document.getElementById('txt' + '_' + j + '_' + column).value = puzzleArrayData[j][column];
+            }else break;
+        }
+        // Print elements on right
+        for(i = column; i< puzzleArrayData[row].length; i++){
+            if(puzzleArrayData[row][i] != 0){
+                document.getElementById('txt' + '_' + row + '_' + i).value = puzzleArrayData[row][i];
+            }else break;
+        }
+
+        // Print elements below
+        for(m = row; m< puzzleArrayData.length; m++){
+            if(puzzleArrayData[m][column] != 0){
+                document.getElementById('txt' + '_' + m + '_' + column).value = puzzleArrayData[m][column];
+            }else break;
+        }
+        // Print elements on left
+        for(k = column; k >= 0; k--){
+            if(puzzleArrayData[row][k] != 0){
+                document.getElementById('txt' + '_' + row + '_' + k).value = puzzleArrayData[row][k];
+            }else break;
+        }
+        // Done!
+
+    }
+}
 
 
