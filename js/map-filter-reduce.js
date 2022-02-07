56b3dc85813445b7a0d9adc7ccefc112de8f2b7c
Cody Hodges <hodges.cody00@gmail.com>
2022-02-07 12:30:54 -0600

    MAP Exercise

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -87,3 +87,85 @@ const userNames = users.reduce((previousValue, currentValue) => {
 }, '')
 console.log('Users names = ' + userNames)
 
+
+
+
+
+
+
+// BONUSES
+const fruits = ["cantaloupe", "orange", "date", "elderberry", "ugli fruit", "pineapple"];
+
+const customers = [
+    {
+        name: "Fred",
+        age: 58,
+        occupation: "Police Officer",
+        noOfPurchases: 4
+    },
+    {
+        name: "Samantha",
+        age: 54,
+        occupation: "Teacher",
+        noOfPurchases: 18
+    },
+    {
+        name: "Charles",
+        age: 38,
+        occupation: "Librarian",
+        noOfPurchases: 9
+    }
+];
+
+const pets = [
+    {
+        name: 'Bud',
+        age: 2,
+        breed: 'Pug'
+    },
+    {
+        name: 'Gabby',
+        age: 10,
+        breed: 'Retriever'
+    },
+    {
+        name: 'Fred',
+        age: 1,
+        breed: 'Lab'
+    },
+    {
+        name: 'Bowser',
+        age: 2,
+        breed: 'Pug'
+    }
+];
+
+const family = [
+    {
+        name: "Pam",
+        gender: "female",
+        age: 29,
+    },
+    {
+        name: "Amelie",
+        gender: "female",
+        age: 10,
+    },
+    {
+        name: "Justin",
+        gender: "male",
+        age: 32,
+    },
+];
+
+
+// Create an array of the first letters of each fruit
+
+
+// Create array of user objects based on the customers array of objects (each user object should just have name and age properties)
+
+// Create an array of civil servant customers (teachers and police officers) containing the same properties as the objects on the customers objects
+
+// Determine the average age of all the customers
+
+// Create a function makeSuperPet() that takes in the pets array as input and returns a single pet object with the following shape...
