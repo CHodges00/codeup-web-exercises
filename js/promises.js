719ea24e608706a30f899ceacb36aac2f2c45405
Cody Hodges <hodges.cody00@gmail.com>
2022-02-07 15:12:11 -0600

    Promises Exercise

--- js/promises.js
+++ js/promises.js
@@ -5,7 +5,7 @@ let username = 'CHodges00'
 function getCommits(username) {
     return fetch(`https://api.github.com/users/${username}/events`)
         .then(response => response.json())
-        
+
         .catch(error => console.error(error))
 }
 
