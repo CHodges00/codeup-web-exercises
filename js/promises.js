5877bb6f0dc9d96621b884f1b4f6b3234256280d
Cody Hodges <hodges.cody00@gmail.com>
2022-02-08 11:58:31 -0600

    Promises Exercises

--- js/promises.js
+++ js/promises.js
@@ -1,22 +1,20 @@
-
 // USERNAME TO PASS
 let username = 'CHodges00'
-// // PROMISE
-// const myPromise = fetch(`https://api.github.com/users/${username}/events`, {headers: {'Authorization': `token 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`}});
-// console.log(myPromise)
-// // RESPONSE
-// myPromise.then(response => response.json());
-//
-// // ERROR
-// myPromise.catch(error => console.error(error));
+
+// let username = $('#input').val()
 
 
-
-fetch(`https://api.github.com/users/${username}/events`, {headers: {'Authorization': `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`}}).then( response => {
-    response.json().then( users => {
-        users.forEach( user => {
-            // do something with each user object...
-            console.log(user);
+// function githubGet() { return
+fetch(`https://api.github.com/users/${username}/events`, {headers: {'Authorization': `GITHUB_TOKEN`}}).then(response => {
+    response.json().then(events => {
+        events.forEach(event => {
+            console.log(response);
+            console.log(event);
         });
     });
 });
+// };
+
+
+// $('#submit').on('click', githubGet())
+
