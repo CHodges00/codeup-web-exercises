9a84df501bb8e8e0eacb67b96c56448f787dae94
Cody Hodges <hodges.cody00@gmail.com>
2022-02-08 14:54:29 -0600

    Promise Exercise

--- js/promises.js
+++ js/promises.js
@@ -3,17 +3,17 @@ function githubGet() {
 
     return fetch(`https://api.github.com/users/${username}/events`, {headers: {'Authorization': `GITHUB_TOKEN`}}).then(response => {
         response.json().then(events => {
-            var localDate = new Date(events[0].created_at.toLocaleString());
-            var now = new Date()
-            var difference = 1
-            console.log('difference ' + difference)
-            console.log('now ' + now.getTime())
-            console.log('push ' + localDate.getTime())
-            console.log((((now.getTime() - localDate.getTime()) / 1000)/60)/60)
-            // console.log('Date of last push = ', events[0].created_at.substring(10, 0))
-            // console.log('Time of last push = ', events[0].created_at.split('T', 5)[1])
-            // console.log('Last push was on :', localDate)
-            // $('#last-push').append(`Your last push was completed on : ${localDate}`)
+            var push = new Date(events[0].created_at.toLocaleString()).getTime();
+            var now = new Date().getTime()
+            const diff = (now - push);
+
+            console.log(push)
+            console.log(now)
+            console.log(diff/1000/60)
+
+
+            $('#last-push').append(`Your last push was completed on : ${localDate}`)
+
         });
     });
 }
