7e99bec00ebc8159b2d039970731b1f3f214196d
Cody Hodges <hodges.cody00@gmail.com>
2022-02-08 15:37:29 -0600

    Practice Exercise

--- js/promises.js
+++ js/promises.js
@@ -3,20 +3,27 @@ function githubGet() {
 
     return fetch(`https://api.github.com/users/${username}/events`, {headers: {'Authorization': `GITHUB_TOKEN`}}).then(response => {
         response.json().then(events => {
-            var push = new Date(events[0].created_at.toLocaleString()).getTime();
-            var now = new Date().getTime()
-            const diff = (now - push);
+            // ZULU TIME/DATE FORMAT RETURNED FROM EVENT
+            let push = events[0].created_at;
+            // TIME OF PUSH IN LOCAL TIME STRING
+            let timeOfPush = new Date(push).toLocaleString()
+            // TIME OF PUSH IN MILLISECONDS
+            let pushTime = new Date(events[0].created_at.toLocaleString()).getTime();
+            // TIME NOW IN MILLISECONDS
+            let now = new Date().getTime();
+            // DIFFERENCE BETWEEN PUSH AND NOW STRTING WITH MILLISECONDS
+            let diff = (((now - pushTime) / 1000) / 60).toFixed(0);
 
-            console.log(push)
-            console.log(now)
-            console.log(diff/1000/60)
+            console.log(timeOfPush.substring(timeOfPush.indexOf(',') + 1))
 
 
-            $('#last-push').append(`Your last push was completed on : ${localDate}`)
-
+            $('#last-push').html('');
+            $('#last-push').append(`Your last push was completed on : ${timeOfPush.substring(0, timeOfPush.indexOf(','))}<br> @ ${timeOfPush.substring(timeOfPush.indexOf(',') + 1)}`);
+            $('#time-since').html('');
+            $('#time-since').append(`This was : ${diff} minutes ago`);
         });
     });
 }
 
-$('#submit').on('click', githubGet);
+$('#submit').one('click', githubGet);
 
