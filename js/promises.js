cc320707e44cebf1cdba7734ebb0604a73c03d12
Cody Hodges <hodges.cody00@gmail.com>
2022-02-08 14:08:48 -0600

    Promises Exercise

--- js/promises.js
+++ js/promises.js
@@ -1,20 +1,22 @@
-// USERNAME TO PASS
-let username = 'CHodges00'
+function githubGet() {
+    let username = $('#input').val()
 
-// let username = $('#input').val()
-
-
-// function githubGet() { return
-fetch(`https://api.github.com/users/${username}/events`, {headers: {'Authorization': `GITHUB_TOKEN`}}).then(response => {
-    response.json().then(events => {
-        events.forEach(event => {
-            console.log(response);
-            console.log(event);
+    return fetch(`https://api.github.com/users/${username}/events`, {headers: {'Authorization': `GITHUB_TOKEN`}}).then(response => {
+        response.json().then(events => {
+            var localDate = new Date(events[0].created_at.toLocaleString());
+            var now = new Date()
+            var difference = 1
+            console.log('difference ' + difference)
+            console.log('now ' + now.getTime())
+            console.log('push ' + localDate.getTime())
+            console.log((((now.getTime() - localDate.getTime()) / 1000)/60)/60)
+            // console.log('Date of last push = ', events[0].created_at.substring(10, 0))
+            // console.log('Time of last push = ', events[0].created_at.split('T', 5)[1])
+            // console.log('Last push was on :', localDate)
+            // $('#last-push').append(`Your last push was completed on : ${localDate}`)
         });
     });
-});
-// };
+}
 
-
-// $('#submit').on('click', githubGet())
+$('#submit').on('click', githubGet);
 
