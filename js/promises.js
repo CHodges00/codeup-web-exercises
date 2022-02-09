4c9cd0b5a47f8fde4e7fbc5ff10839eb90e87777
Cody Hodges <hodges.cody00@gmail.com>
2022-02-09 08:56:57 -0600

    Promises Exercise

--- js/promises.js
+++ js/promises.js
@@ -38,7 +38,6 @@ function wait(milliseconds) {
     });
 }
 
-
 // wait(2000).then(() => alert('Welcome, the only thing to do here is search how long it has been since your last GitHub push........ENJOY !'));
 // wait(20000).then(() => prompt(`You've been here for a while, enter your email to sign up for our newsletter !`));
 
